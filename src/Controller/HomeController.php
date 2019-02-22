2b1f6d84541ef1c10b454d6dcd4070424f4309f5
Jeremie Soffichiti <jeremie.soffichiti@gmail.com>
2019-02-22 18:33:34 +0100

    export

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PropertyRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends  AbstractController {
+
+
+    /**
+     * @param PropertyRepository $property
+     * @return Response
+     */
+    public function index(PropertyRepository $property): Response
+    {
+        $properties = $property->findLatest();
+
+        dump($properties);
+
+        return $this->render('pages/home.html.twig', [
+            'properties' => $properties
+        ]);
+    }
+}
